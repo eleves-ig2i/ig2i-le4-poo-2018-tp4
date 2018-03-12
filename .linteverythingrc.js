2bc94eb7ff2ab8e0fae759ee4671877e65feeb90
Sébastien CORNUEL <sbastien.cornuel@gmail.com>
2018-03-12 17:31:44 +0100

    Init Comit

---
+++ .linteverythingrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	ignore: [
+		'node_modules',
+		'package-lock.json',
+		'.git',
+		'.travis.yml',
+		'build',
+		'ig2i-le4-poo-2018-tp4.pdf',
+		'db_routage_with_data.sql',
+		'db_scripts_instances.zip',
+		'nbproject/project.xml'
+	]
+};
